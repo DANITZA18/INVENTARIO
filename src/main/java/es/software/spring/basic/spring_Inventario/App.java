fa3eedf0856fa2cedabfbddce4f462be5a39d705
danitza18 <danitzamarlenechurasocujayo@gmail.com>
2019-10-01 13:57:38 -0500

    creacion de proyecto

---
+++ src/main/java/es/software/spring/basic/spring_Inventario/App.java
@@ -0,0 +1,13 @@
+package es.software.spring.basic.spring_Inventario;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
